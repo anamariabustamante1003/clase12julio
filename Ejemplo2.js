77eeacdb30a9331cd8f72f6f8499f0e72eeea996
anamariabustamante1003 <30291135+anamariabustamante1003@users.noreply.github.com>
2017-07-31 17:41:01 -0500

    Create Ejemplo2.js

---
+++ Ejemplo2.js
@@ -0,0 +1,8 @@
+alert("Hola mundo");
+if(confirm("todo ray")){
+	//si se cumple la condición
+	alert("genial, me alegro que vas bien, espero que sigas asi, manos a la obra  ");
+}else{
+	//si no se cumple la condicion 
+	alert("OKEY");
+}
